6cdac521244f4059bcf403ca03791f8d23920d44
JQuinnie <jenn8866@gmail.com>
2018-07-21 19:18:38 -0600

    Update chain of request promises to get UV Index

--- controllers/geocode-api-routes.js
+++ controllers/geocode-api-routes.js
@@ -12,12 +12,10 @@ module.exports = function(app) {
   let searchDT = "2018-01-24T19:13:14.138Z";
 
   const geoURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchCity}&key=${GEOKEY}`;
-  const openUVURL = `https://api.openuv.io/api/v1/uv?lat=${searchLat}&lng=${searchLng}&alt=${searchAlt}&x-access-token=${OPENUVKEY}`;
 
   // test geocode API with input of city, state to latitude, longitude
   requestProm(geoURL, function(error, response, body) {
     if (error) throw error;
-
     if (response.statusCode === 200) {
       // console.log(JSON.parse(body).results[0]);
       let city = JSON.parse(body).results[0].address_components[0].short_name;
@@ -28,25 +26,39 @@ module.exports = function(app) {
       );
     }
   })
-    .then(function(response) {
-      let options = {
-        method: "GET",
-        url: `https://api.openuv.io/api/v1/uv?lat=${searchLat}&lng=${searchLng}&alt=${searchAlt}&dt=${searchDT}`,
-        headers: {
-          "content-type": "application/json",
-          "x-access-token": OPENUVKEY
-        }
-      };
-      // test openUV API with input of latitude, longitude, altitude from GeoCodeURL
-      request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-
+    .then(function() {
+      const geoAltURL = `https://maps.googleapis.com/maps/api/elevation/json?locations=${searchLat},${searchLng}&key=${GEOKEY}`;
+      return requestProm(geoAltURL, function(error, response, body) {
+        if (error) throw error;
         if (response.statusCode === 200) {
-          console.log(JSON.parse(body));
-          console.log("UV Max: " + JSON.parse(body).result.uv_max);
-          // safe_exposure_time will return null if there are no sun or its before/after sunset, use uv_max_time to test
+          // searchAlt = JSON.parse(body);
+          searchAlt = JSON.parse(body).results[0].elevation;
+          console.log("Altitude: " + searchAlt);
         }
-      });
+      })
+        .then(function() {
+          // test openUV API with input of latitude, longitude, altitude from GeoCodeURL
+          let options = {
+            method: "GET",
+            url: `https://api.openuv.io/api/v1/uv?lat=${searchLat}&lng=${searchLng}&alt=${searchAlt}&dt=${searchDT}`,
+            headers: {
+              "content-type": "application/json",
+              "x-access-token": OPENUVKEY
+            }
+          };
+
+          request(options, function(error, response, body) {
+            if (error) throw new Error(error);
+            if (response.statusCode === 200) {
+              console.log(JSON.parse(body));
+              console.log("UV Max: " + JSON.parse(body).result.uv_max);
+              // safe_exposure_time will return null if there are no sun or its before/after sunset, use uv_max_time to test
+            }
+          });
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     })
     .catch(function(err) {
       console.log(err);
