621bd942cc757a4936eb7d9f9b87c7480c31b011
Jenn C <jenn8866@gmail.com>
2021-12-29 17:24:06 -0600

    Remove material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,13 @@
 import React from 'react';
-import InputBar from './components/InputBar';
+import InputForm from './components/InputForm';
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Box, Container, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const App = () => {
-  const classes = useStyles();
-
-  return (
-    <Box>
-      <div className="App">
-        <Container>
-          <h2>Will I Tan?</h2>
-          <FormControl className={classes.formControl}>
-            <InputLabel>Select Type</InputLabel>
-            <Select>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
-          <InputBar />
-        </Container>
-      </div>
-    </Box>
-  );
-};
+const App = () => (
+  <div className="App">
+    <h2>Will I Tan?</h2>
+    <InputForm />
+  </div>
+);
 
 export default App;
