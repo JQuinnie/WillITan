8a3f3788a954746cd98e79fbaaf13ab8da79a672
Jenn C <jenn8866@gmail.com>
2021-12-29 16:10:18 -0600

    Mock out geocode call

--- services/geocodeService.js
+++ services/geocodeService.js
@@ -1,5 +1,9 @@
-const axios = require('axios');
 const _ = require('lodash');
+const axios = require('axios');
+
+const geocodeResponse = require('../mockData/geocodeSampleResponse.json');
+
+// TODO: elevationAPI returns latLng, so geocode call might not be needed
 
 const geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
 
@@ -8,16 +12,20 @@ if (!geocodeUrl) throw new Error('geocodeUrl is missing');
 const getLatLng = async (address) => {
   let msg;
 
-  try {
-    msg = `Calling geocode API for ${address}`;
-    const response = await axios.get(geocodeUrl, {
-      params: {
-        key: process.env.GEOCODE_KEY,
-        address,
-      },
-    });
+  // TODO: add a logger
 
-    const { results } = response.data;
+  try {
+    // msg = `Calling geocode API for ${address}`;
+    // const response = await axios.get(geocodeUrl, {
+    //   params: {
+    //     key: process.env.GOOGLE_MAPS_API_KEY,
+    //     address,
+    //   },
+    // });
+
+    // const { results } = response.data;
+
+    const { results } = geocodeResponse; // TODO: remove mock and uncomment actual API call
     if (!results) {
       throw new Error('No data returned from Geocode API');
     }
@@ -26,6 +34,7 @@ const getLatLng = async (address) => {
     console.log(`Geocode API return ${JSON.stringify(latlng)} for ${address}`);
     return { location: latlng };
   } catch (err) {
+    // TODO: format error to not be HTML, better error
     throw new Error(`${msg} - ${err.message}`);
   }
 };
